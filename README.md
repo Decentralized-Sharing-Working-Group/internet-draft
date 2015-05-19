de6643fe45243fb8b0d053a42063f6b6f8e87972
Michiel de Jong <michiel@unhosted.org>
2015-05-19 16:20:21 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Contributing
+
+This repository is where we keep track of the versioning of our internet draft:
+
+    https://tools.ietf.org/id/draft-dejong-decentralized-sharing-**.txt
+
+If you would like to suggest a change, you can open an issue
+on https://github.com/Decentralized-Sharing-Working-Group/internet-draft/issues and discuss your proposal with the spec
+authors.
+
+# Build
+
+To build for instance version 04, run:
+
+````
+    node build.js > draft-dejong-decentralized-sharing-04.txt
+````
+
---
+++ build.js
@@ -0,0 +1,25 @@
+var fs = require('fs'),
+  lines = fs.readFileSync('source.txt').toString().split('\n'),
+  breaker1a = '\n\nde Jong                                                         [Page ',
+  breaker1b = '\n\nde Jong                                                        [Page ',
+  breaker2 = ']',
+  breaker3 = '\n\nInternet-Draft           Decentralized Sharing               May 2015\n\n',
+  page = 1, line = 0;
+
+while (line < lines.length) {
+  console.log(lines[line]);
+  if (lines[line].length > 72) {
+    console.log('123456789012345678901234567890123456789012345678901234567890123456789012*****');
+    break;
+  }
+  line++;
+  if (line > 7 && line%43 === 7) {
+    console.log((page < 10 ? breaker1a : breaker1b) + page + breaker2 + (line < lines.length ? breaker3 : ''));
+    page++;
+  }
+}
+while (line%43 !== 7) {
+  console.log('');
+  line++;
+}
+console.log((page < 10 ? breaker1a : breaker1b) + page + breaker2 + (line < lines.length ? breaker3 : ''));
